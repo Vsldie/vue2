a6643d2e9e23a38385ee3f674dd45d552f0def1a
gr411_vasle <svetlanavasil7@mail.ru>
2023-03-14 09:47:08 +0700

    практическая 2

--- js/main.js
+++ js/main.js
@@ -29,11 +29,15 @@ Vue.component('component', {
 
     },
     mounted() {
+        this.column_1 = JSON.parse(localStorage.getItem('column_1')) || [];
+        this.column_2 = JSON.parse(localStorage.getItem('column_2')) || [];
+        this.column_3 = JSON.parse(localStorage.getItem('column_3')) || [];
         eventBus.$on('addColumn_1', ColumnCard => {
 
             if (this.column_1.length < 3) {
                 this.errors.length = 0
                 this.column_1.push(ColumnCard)
+                this.saveColumn_1();
             } else {
                 this.errors.length = 0
                 this.errors.push()
@@ -44,6 +48,8 @@ Vue.component('component', {
                 this.errors.length = 0
                 this.column_2.push(ColumnCard)
                 this.column_1.splice(this.column_1.indexOf(ColumnCard), 1)
+                this.saveColumn_1();
+                this.saveColumn_2();
             } else {
                 this.errors.length = 0
                 this.errors.push()
@@ -52,8 +58,21 @@ Vue.component('component', {
         eventBus.$on('addColumn_3', ColumnCard => {
             this.column_3.push(ColumnCard)
             this.column_2.splice(this.column_2.indexOf(ColumnCard), 1)
+            this.saveColumn_2();
+            this.saveColumn_3();
 
         })
+    },
+    methods: {
+        saveColumn_1(){
+            localStorage.setItem('column_1', JSON.stringify(this.column_1));
+        },
+        saveColumn_2(){
+            localStorage.setItem('column_2', JSON.stringify(this.column_2));
+        },
+        saveColumn_3(){
+            localStorage.setItem('column_3', JSON.stringify(this.column_3));
+        }
     }
 })
 
